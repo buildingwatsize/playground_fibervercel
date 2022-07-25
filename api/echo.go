5b779a6dc47d6ef92bafeeb54945ef6c1b4e1619
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2022-07-25 16:23:57 +0700

    try to turn the config like a module

--- api/echo.go
+++ api/echo.go
@@ -3,32 +3,12 @@ package handler
 import (
 	"log"
 	"net/http"
-	"os/exec"
+
+	"playground/config"
 
 	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
-	"github.com/spf13/viper"
 )
-
-func init() {
-	out, _ := exec.Command("ls", "-la").Output()
-	log.Println(string(out))
-	out2, lsErr := exec.Command("ls", "-la", "../").Output()
-	if lsErr != nil {
-		log.Fatal(lsErr)
-	}
-	log.Println(string(out2))
-
-	viper.AddConfigPath("./_env")
-	viper.SetConfigName("env")
-	viper.SetConfigType("json")
-
-	viper.AutomaticEnv()
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Panicf("Fatal error config file: %s \n", err)
-	}
-}
 
 func EchoHandler(w http.ResponseWriter, r *http.Request) {
 	adaptor.FiberHandlerFunc(EchoAPI)(w, r)
@@ -39,17 +19,7 @@ func EchoAPI(c *fiber.Ctx) error {
 
 	log.Println("Reached: Echo API")
 
-	config := newConfiguration()
-	log.Printf("%+v\n", config)
-	return c.JSON(config)
-}
-
-type Configuration struct {
-	FirstTimeMessage string
-}
-
-func newConfiguration() Configuration {
-	return Configuration{
-		FirstTimeMessage: viper.GetString("FIRST_TIME_MESSAGE"),
-	}
+	conf := config.NewConfiguration()
+	log.Printf("%+v\n", conf)
+	return c.JSON(conf)
 }
